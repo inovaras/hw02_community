3e5b0a2efead7f9aa10d6186ae165867507eccbf
Albina Gilmutdinova <inovaras@mail.ru>
2023-01-27 22:56:27 +0500

    added folder yatube

---
+++ yatube/posts/admin.py
@@ -0,0 +1,28 @@
+from django.contrib import admin
+from .models import Group
+from .models import Post
+
+
+class PostAdmin(admin.ModelAdmin):
+    # Перечисляем поля, которые должны отображаться в админке
+    list_display = ('pk', 'text', 'pub_date', 'author', 'group')
+    list_editable = ('group',)
+    # Добавляем интерфейс для поиска по тексту постов
+    search_fields = ('text',)
+    # Добавляем возможность фильтрации по дате
+    list_filter = ('pub_date',)
+    empty_value_display = '-пусто-'
+
+
+class GroupAdmin(admin.ModelAdmin):
+    # Перечисляем поля, которые должны отображаться в админке
+    list_display = ('title', 'slug', 'description')
+    # Добавляем интерфейс для поиска по тексту постов
+    search_fields = ('description',)
+    empty_value_display = '-empty-'
+
+
+# При регистрации модели Post источником конфигурации для неё назначаем
+# класс PostAdmin
+admin.site.register(Group, GroupAdmin)
+admin.site.register(Post, PostAdmin)
---
+++ yatube/posts/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import get_object_or_404
+from django.shortcuts import render
+
+# Импортируем модель, чтобы обратиться к ней
+from .models import Post, Group
+
+
+def index(request):
+    posts = Post.objects.order_by('-pub_date')[:10]
+    context = {
+        'posts': posts,
+    }
+    return render(request, 'posts/index.html', context)
+
+
+# View-функция для страницы сообщества:
+def group_posts(request, slug):
+    group = get_object_or_404(Group, slug=slug)
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
+    context = {
+        'group': group,
+        'posts': posts,
+    }
+    return render(request, 'posts/group_list.html', context)
