b2696301c8290f665f36bab6f278a342487754e8
Albina Gilmutdinova <inovaras@mail.ru>
2023-02-20 02:52:28 +0500

    log_out

--- yatube/yatube/urls.py
+++ yatube/yatube/urls.py
@@ -20,4 +20,6 @@ urlpatterns = [
     path('', include('posts.urls', namespace='posts')),
     path('group/<slug:slug>/', include('posts.urls', namespace='posts')),
     path('admin/', admin.site.urls),
+    path('auth/', include('users.urls')),
+    path('auth/', include('django.contrib.auth.urls')),
 ]
