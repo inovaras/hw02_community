1965625e62db658d7a8cdbde18660ebba09ba057
Albina Gilmutdinova <inovaras@mail.ru>
2023-02-20 15:37:35 +0500

    added posts const

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import get_object_or_404
 from django.shortcuts import render
 from .models import Post, Group
+from .consts import POSTS_NUMBERS
 
 
 def index(request):
-    posts = Post.objects.all()[:10]
+    posts = Post.objects.all()[:POSTS_NUMBERS]
     print(posts)
     context = {
         'posts': posts,
@@ -14,7 +15,7 @@ def index(request):
 
 def group_posts(request, slug):
     group = get_object_or_404(Group, slug=slug)
-    posts = Post.objects.filter(group=group).all()[:10]
+    posts = Post.objects.filter(group=group).all()[:POSTS_NUMBERS]
     context = {
         'group': group,
         'posts': posts,
