f108490462c6261b0125ad41cb1ece11839ce4e4
Albina Gilmutdinova <inovaras@mail.ru>
2023-02-21 19:47:06 +0500

    added paginator

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,15 +1,20 @@
+from django.core.paginator import Paginator
+from django.conf import settings
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import get_object_or_404
 from django.shortcuts import render
 from .models import Post, Group
-from django.conf import settings
-from django.contrib.auth.decorators import login_required
+
 
 
 @login_required()
 def index(request):
-    posts = Post.objects.all()[: settings.POSTS_NUMBERS]
+    post_list = Post.objects.all()
+    paginator = Paginator(post_list, 10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
     context = {
-        'posts': posts,
+        'page_obj': page_obj,
     }
     return render(request, 'posts/index.html', context)
 
